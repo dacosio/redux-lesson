2a64b7fd0bc9ef5a5b87667de36ea5f903c0d813
Don Apollo Cosio <dacosio@gmail.com>
2023-04-24 22:34:23 -0700

    rtk query for users

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -1,11 +1,12 @@
 import { useSelector } from "react-redux";
 import { selectUserById } from "../users/usersSlice";
-import { useGetPostsByUserIdQuery } from "../posts/postsSlice";
 import { Link, useParams } from "react-router-dom";
+import { useGetPostsByUserIdQuery } from "../posts/postsSlice";
 
 const UserPage = () => {
   const { userId } = useParams();
   const user = useSelector((state) => selectUserById(state, Number(userId)));
+
   const {
     data: postsForUser,
     isLoading,
--- src/features/users/UsersList.js
+++ src/features/users/UsersList.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import { selectAllUsers } from "./usersSlice";
 import { Link } from "react-router-dom";
@@ -6,17 +5,16 @@ import { Link } from "react-router-dom";
 const UsersList = () => {
   const users = useSelector(selectAllUsers);
 
-  const renderedUsers = users.map((user) => {
-    return (
-      <li key={user.id}>
-        <Link to={`/user/${user.id}`}>{user.name}</Link>
-      </li>
-    );
-  });
+  const renderedUsers = users.map((user) => (
+    <li key={user.id}>
+      <Link to={`/user/${user.id}`}>{user.name}</Link>
+    </li>
+  ));
 
   return (
     <section>
       <h2>Users</h2>
+
       <ul>{renderedUsers}</ul>
     </section>
   );
